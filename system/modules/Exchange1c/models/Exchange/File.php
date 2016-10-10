8e3681bc87c0370f6420938ff9644184b4b806b2
Alexey Krupsky <admin@inji.ru>
2016-10-10 10:52:46 +0700

    fix save filesize

--- system/modules/Exchange1c/models/Exchange/File.php
+++ system/modules/Exchange1c/models/Exchange/File.php
@@ -11,19 +11,20 @@
 
 namespace Exchange1c\Exchange;
 
-class File extends \Model
-{
-    public static $cols = [
-        'name' => ['type' => 'text'],
-        'size' => ['type' => 'number'],
-        'date_create' => ['type' => 'dateTime'],
-    ];
-    public static $dataManagers = [
-        'manager' => [
-            'cols' => [
-                'name', 'size', 'date_create'
-            ],
-        ]
-    ];
+class File extends \Model {
+
+  public static $cols = [
+      'name' => ['type' => 'text'],
+      'size' => ['type' => 'number'],
+      'status' => ['type' => 'text'],
+      'date_create' => ['type' => 'dateTime'],
+  ];
+  public static $dataManagers = [
+      'manager' => [
+          'cols' => [
+              'name', 'size', 'date_create'
+          ],
+      ]
+  ];
 
 }
--- system/modules/Exchange1c/objects/Mode/File.php
+++ system/modules/Exchange1c/objects/Mode/File.php
@@ -11,35 +11,37 @@
 
 namespace Exchange1c\Mode;
 
-class File extends \Exchange1c\Mode
-{
-    public function process()
-    {
-        $dir = $this->exchange->path;
-        \Tools::createDir($dir);
-        $file = new \Exchange1c\Exchange\File();
-        $file->name = $_GET['filename'];
-        $file->exchange_id = $this->exchange->id;
-        $file->size = ceil(filesize($dir . '/' . $_GET['filename']));
-        $file->save();
+class File extends \Exchange1c\Mode {
 
-        if (strpos($_GET['filename'], '/') !== false) {
-            $subDir = substr($_GET['filename'], 0, strrpos($_GET['filename'], "/") + 1);
-            \Tools::createDir($dir . '/' . $subDir);
-        }
-        $status = 'success';
-        $text = '';
-        if (false === file_put_contents($dir . '/' . $_GET['filename'], file_get_contents("php://input"))) {
-            $status = 'failure';
-            $text = 'Fail on save file: ' . $_GET['filename'];
-        }
-        if (strpos($_GET['filename'], '1cbitrix') !== false) {
-            $data = new \SimpleXMLElement(file_get_contents($dir . '/' . $_GET['filename']));
-            $orders = new \Exchange1c\Parser\Orders($data);
-            $orders->process();
-        }
-        \App::$cur->exchange1c->response($status, $text, false);
-        $this->end($status);
+  public function process() {
+    $dir = $this->exchange->path;
+    \Tools::createDir($dir);
+    $file = new \Exchange1c\Exchange\File();
+    $file->name = $_GET['filename'];
+    $file->exchange_id = $this->exchange->id;
+    $file->status = 'pending';
+    $file->save();
+
+    if (strpos($_GET['filename'], '/') !== false) {
+      $subDir = substr($_GET['filename'], 0, strrpos($_GET['filename'], "/") + 1);
+      \Tools::createDir($dir . '/' . $subDir);
     }
+    $text = '';
+    if (false === file_put_contents($dir . '/' . $_GET['filename'], file_get_contents("php://input"))) {
+      $text = 'Fail on save file: ' . $_GET['filename'];
+      $file->status = 'failure';
+    } else {
+      $file->size = ceil(filesize($dir . '/' . $_GET['filename']));
+      $file->status = 'success';
+    }
+    $file->save();
+    if (strpos($_GET['filename'], '1cbitrix') !== false) {
+      $data = new \SimpleXMLElement(file_get_contents($dir . '/' . $_GET['filename']));
+      $orders = new \Exchange1c\Parser\Orders($data);
+      $orders->process();
+    }
+    \App::$cur->exchange1c->response($file->status, $text, false);
+    $this->end($file->status);
+  }
 
 }
