47aaebb07054edf869021472c11d2b5f3d6d2c8f
Alexey Krupsky <admin@inji.ru>
2016-11-16 13:39:41 +0700

    inproove input generation

--- system/modules/Ui/widgets/Form/text.php
+++ system/modules/Ui/widgets/Form/text.php
@@ -1,10 +1,25 @@
-
 <?php
+
 echo empty($options['noContainer']) ? '<div class="form-group">' : '';
 echo $label !== false ? "<label>{$label}</label>" : '';
-?>
-<input <?= !empty($options['required']) ? 'required' : ''; ?> type ="text" placeholder="<?= !empty($options['placeholder']) ? $options['placeholder'] : ''; ?>" class="form-control" name = '<?= $name; ?>' value = '<?= !empty($options['value']) ? addcslashes($options['value'], "'") : (!empty($form->userDataTree[$name]) ? addcslashes($form->userDataTree[$name], "'") : ''); ?>' />
-<?php
+$attributes = [
+    'type' => 'text',
+    'name' => $name,
+    'class' => 'form-control',
+    'value' => !empty($options['value']) ? addcslashes($options['value'], "'") : (!empty($form->userDataTree[$name]) ? addcslashes($form->userDataTree[$name], "'") : '')
+];
+if (!empty($options['disabled'])) {
+  $attributes['disabled'] = 'disabled';
+}
+if (!empty($options['placeholder'])) {
+  $attributes['placeholder'] = $options['placeholder'];
+}
+if (!empty($options['checked'])) {
+  $attributes['checked'] = 'checked';
+}
+if (!empty($options['attributes'])) {
+  $attributes = array_merge($attributes, $options['attributes']);
+}
+echo Html::el('input', $attributes, '', null);
 echo!empty($options['helpText']) ? "<div class='help-block'>{$options['helpText']}</div>" : '';
 echo empty($options['noContainer']) ? '</div>' : '';
-?>
--- system/modules/Ui/widgets/Form/textarea.php
+++ system/modules/Ui/widgets/Form/textarea.php
@@ -1,4 +1,17 @@
-<div class="form-group">
-    <label><?= $label; ?></label>
-    <textarea <?= !empty($options['required']) ? 'required' : ''; ?> class="form-control <?= !empty($options['class']) ? $options['class'] : ''; ?>" name = '<?= $name; ?>'><?= !empty($options['value']) ? $options['value'] : (!empty($form->userDataTree[$name]) ? addcslashes($form->userDataTree[$name], "'") : ''); ?></textarea>
-</div>
+  <?php
+
+  echo empty($options['noContainer']) ? '<div class="form-group">' : '';
+  echo $label !== false ? "<label>{$label}</label>" : '';
+  $attributes = [
+      'name' => $name,
+      'class' => "form-control " . (!empty($options['class']) ? $options['class'] : '')
+  ];
+  if (!empty($options['required'])) {
+    $attributes['required'] = 'required';
+  }
+  if (!empty($options['attributes'])) {
+    $attributes = array_merge($attributes, $options['attributes']);
+  }
+  echo Html::el('textarea', $attributes, !empty($options['value']) ? $options['value'] : (!empty($form->userDataTree[$name]) ? addcslashes($form->userDataTree[$name], "'") : ''));
+  echo empty($options['noContainer']) ? '</div>' : '';
+  
