3fd3a83ae6c8e2181863fa5a9f4a0d72f33ab12c
Alexey Krupsky <admin@inji.ru>
2016-11-24 02:22:29 +0700

    fix default start and limit

--- system/modules/Ui/Controllers/DataManagerController.php
+++ system/modules/Ui/Controllers/DataManagerController.php
@@ -8,257 +8,249 @@
  * @copyright 2015 Alexey Krupskiy
  * @license https://github.com/injitools/cms-Inji/blob/master/LICENSE
  */
-class DataManagerController extends Controller
-{
-    public function parseRequest()
-    {
-        $return = [];
-        $return['params'] = UserRequest::get('params', 'array', []);
+class DataManagerController extends Controller {
 
-        $item = UserRequest::get('modelName', 'string', '');
-        if (!$item) {
-            $item = UserRequest::get('item', 'string', '');
-        }
+  public function parseRequest() {
+    $return = [];
+    $return['params'] = UserRequest::get('params', 'array', []);
 
-        if (strpos($item, ':')) {
-            $raw = explode(':', $item);
-            $return['modelName'] = $raw[0];
-            $return['model'] = $return['modelName']::get($raw[1], $return['modelName']::index(), $return['params']);
-        } else {
-            $return['modelName'] = $item;
-            $return['model'] = null;
-        }
-
-        if (!empty($return['params']['relation'])) {
-            $relation = $return['modelName']::getRelation($return['params']['relation']);
-            if (!empty($relation['type']) && $relation['type'] == 'relModel') {
-                $return['modelName'] = $relation['relModel'];
-            } else {
-                $return['modelName'] = $relation['model'];
-            }
-        }
-        $return['params']['filters'] = UserRequest::get('filters', 'array', []);
-        $return['params']['sortered'] = UserRequest::get('sortered', 'array', []);
-        $return['params']['mode'] = UserRequest::get('mode', 'string', '');
-        $return['params']['all'] = UserRequest::get('all', 'bool', false);
-
-        $return['key'] = UserRequest::get('key', 'int', 0);
-        $return['col'] = UserRequest::get('col', 'string', '');
-        $return['col_value'] = UserRequest::get('col_value', 'string', '');
-
-        $return['action'] = UserRequest::get('action', 'string', '');
-        $return['ids'] = trim(UserRequest::get('ids', 'string', ''), ',');
-        $return['adInfo'] = UserRequest::get('adInfo', 'array', []);
-
-        $return['download'] = UserRequest::get('download', 'bool', false);
-        $return['silence'] = UserRequest::get('silence', 'bool', false);
-
-        $return['managerName'] = UserRequest::get('managerName', 'string', 'manager');
-        if (!$return['managerName']) {
-            $return['managerName'] = 'manager';
-        }
-
-        return $return;
+    $item = UserRequest::get('modelName', 'string', '');
+    if (!$item) {
+      $item = UserRequest::get('item', 'string', '');
     }
 
-    public function indexAction()
-    {
-        $result = new Server\Result();
-
-        ob_start();
-
-        $request = $this->parseRequest();
-
-        $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
-        $dataManager->draw($request['params'], $request['model']);
-
-        $result->content = ob_get_contents();
-
-        ob_end_clean();
-
-        $result->send();
+    if (strpos($item, ':')) {
+      $raw = explode(':', $item);
+      $return['modelName'] = $raw[0];
+      $return['model'] = $return['modelName']::get($raw[1], $return['modelName']::index(), $return['params']);
+    } else {
+      $return['modelName'] = $item;
+      $return['model'] = null;
     }
 
-    public function loadRowsAction()
-    {
-        $result = new Server\Result();
-        $result->content = [];
+    if (!empty($return['params']['relation'])) {
+      $relation = $return['modelName']::getRelation($return['params']['relation']);
+      if (!empty($relation['type']) && $relation['type'] == 'relModel') {
+        $return['modelName'] = $relation['relModel'];
+      } else {
+        $return['modelName'] = $relation['model'];
+      }
+    }
+    $return['params']['filters'] = UserRequest::get('filters', 'array', []);
+    $return['params']['sortered'] = UserRequest::get('sortered', 'array', []);
+    $return['params']['mode'] = UserRequest::get('mode', 'string', '');
+    $return['params']['all'] = UserRequest::get('all', 'bool', false);
 
-        ob_start();
+    $return['key'] = UserRequest::get('key', 'int', 0);
+    $return['col'] = UserRequest::get('col', 'string', '');
+    $return['col_value'] = UserRequest::get('col_value', 'string', '');
 
-        $request = $this->parseRequest();
+    $return['action'] = UserRequest::get('action', 'string', '');
+    $return['ids'] = trim(UserRequest::get('ids', 'string', ''), ',');
+    $return['adInfo'] = UserRequest::get('adInfo', 'array', []);
 
-        $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
-        if ($request['download']) {
+    $return['download'] = UserRequest::get('download', 'bool', false);
+    $return['silence'] = UserRequest::get('silence', 'bool', false);
 
-            ini_set('memory_limit', '2000M');
-            set_time_limit(0);
-
-            $request['params']['all'] = true;
-            $request['params']['download'] = true;
-            ob_end_clean();
-            header('Content-Encoding: UTF-8');
-            header("Content-Type: text/csv");
-            header("Content-Disposition: attachment; filename=" . $request['modelName']::$objectName . '.csv');
-            echo "\xEF\xBB\xBF"; // UTF-8 BOM
-
-
-            $cols = $dataManager->getCols();
-            $cols = array_slice($cols, 1);
-            $endRow = true;
-            foreach ($cols as $colName => $options) {
-                if (!$endRow) {
-                    echo ";";
-                }
-                $endRow = false;
-                echo '"' . $options['label'] . '"';
-            }
-            echo "\n";
-            $endRow = true;
-        }
-        if (!$request['params']['all']) {
-            $pages = $dataManager->getPages($request['params'], $request['model']);
-            $request['params']['page'] = $pages->params['page'];
-            $request['params']['limit'] = $pages->params['limit'];
-        }
-        $rows = $dataManager->getRows($request['params'], $request['model']);
-        foreach ($rows as $row) {
-            if ($request['download']) {
-                $row = array_slice($row, 1, -1);
-                foreach ($row as $col) {
-                    if (!$endRow) {
-                        echo ";";
-                    }
-                    $endRow = false;
-                    echo '"' . str_replace(["\n", '"'], ['“'], $col) . '"';
-                }
-                echo "\n";
-                $endRow = true;
-            } else {
-                Ui\Table::drawRow($row);
-            }
-        }
-        if ($request['download']) {
-            exit();
-        }
-
-        $result->content['rows'] = ob_get_contents();
-        ob_clean();
-
-        $result->content['pages'] = '';
-
-        if (isset($pages) && $pages) {
-            if ($pages) {
-                $pages->draw();
-                echo '<div style="background:#fff;">записей: <b>' . $pages->options['count'] . '</b>. страница <b>' . $pages->params['page'] . '</b> из <b>' . $pages->params['pages'] . '</b></div>';
-            }
-            $result->content['pages'] = ob_get_contents();
-            ob_end_clean();
-        }
-        $result->send();
+    $return['managerName'] = UserRequest::get('managerName', 'string', 'manager');
+    if (!$return['managerName']) {
+      $return['managerName'] = 'manager';
     }
 
-    public function loadCategorysAction()
-    {
-        $result = new Server\Result();
+    return $return;
+  }
 
-        ob_start();
+  public function indexAction() {
+    $result = new Server\Result();
 
-        $request = $this->parseRequest();
+    ob_start();
 
-        $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
-        $dataManager->drawCategorys();
+    $request = $this->parseRequest();
 
-        $result->content = ob_get_contents();
-        ob_end_clean();
+    $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
+    $dataManager->draw($request['params'], $request['model']);
 
-        $result->send();
-    }
+    $result->content = ob_get_contents();
 
-    public function delRowAction()
-    {
+    ob_end_clean();
 
-        $request = $this->parseRequest();
+    $result->send();
+  }
 
-        $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
+  public function loadRowsAction() {
+    $result = new Server\Result();
+    $result->content = [];
 
-        if ($dataManager->checkAccess()) {
-            $model = $request['modelName']::get($request['key'], $request['modelName']::index(), $request['params']);
-            if ($model) {
-                $model->delete($request['params']);
-            }
+    ob_start();
+
+    $request = $this->parseRequest();
+
+    $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
+    if ($request['download']) {
+
+      ini_set('memory_limit', '2000M');
+      set_time_limit(0);
+
+      $request['params']['all'] = true;
+      $request['params']['download'] = true;
+      ob_end_clean();
+      header('Content-Encoding: UTF-8');
+      header("Content-Type: text/csv");
+      header("Content-Disposition: attachment; filename=" . $request['modelName']::$objectName . '.csv');
+      echo "\xEF\xBB\xBF"; // UTF-8 BOM
+
+
+      $cols = $dataManager->getCols();
+      $cols = array_slice($cols, 1);
+      $endRow = true;
+      foreach ($cols as $colName => $options) {
+        if (!$endRow) {
+          echo ";";
         }
-        $result = new Server\Result();
-        $result->successMsg = empty($request['silence']) ? 'Запись удалена' : '';
-        $result->send();
+        $endRow = false;
+        echo '"' . $options['label'] . '"';
+      }
+      echo "\n";
+      $endRow = true;
     }
-
-    public function updateRowAction()
-    {
-
-        $request = $this->parseRequest();
-
-        $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
-
-        if ($dataManager->checkAccess()) {
-            $model = $request['modelName']::get($request['key'], $request['modelName']::index(), $request['params']);
-            if ($model) {
-                $model->{$request['col']} = $request['col_value'];
-                $model->save($request['params']);
-            }
+    if (!$request['params']['all']) {
+      $pages = $dataManager->getPages($request['params'], $request['model']);
+      $request['params']['page'] = !empty($pages->params['page']) ? $pages->params['page'] : $dataManager->page;
+      $request['params']['limit'] = !empty($pages->params['limit']) ? $pages->params['limit'] : $dataManager->limit;
+    }
+    $rows = $dataManager->getRows($request['params'], $request['model']);
+    foreach ($rows as $row) {
+      if ($request['download']) {
+        $row = array_slice($row, 1, -1);
+        foreach ($row as $col) {
+          if (!$endRow) {
+            echo ";";
+          }
+          $endRow = false;
+          echo '"' . str_replace(["\n", '"'], ['“'], $col) . '"';
         }
-        $result = new Server\Result();
-        $result->successMsg = empty($request['silence']) ? 'Запись Обновлена' : '';
-        $result->send();
+        echo "\n";
+        $endRow = true;
+      } else {
+        Ui\Table::drawRow($row);
+      }
+    }
+    if ($request['download']) {
+      exit();
     }
 
-    public function groupActionAction()
-    {
-        $request = $this->parseRequest();
-        $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
-        $result = new Server\Result();
-        $result->success = false;
-        $result->content = 'Не удалось выполнить операцию';
-        if ($dataManager->checkAccess()) {
-            $ids = trim($request['ids'], ' ,');
-            if ($request['action'] && $ids) {
-                $actions = $dataManager->getActions();
-                if (!empty($actions[$request['action']])) {
-                    $actionParams = $actions[$request['action']];
-                    if (!empty($actionParams['access']['groups']) && !in_array(\Users\User::$cur->group_id, $actionParams['access']['groups'])) {
-                        $result->content = 'У вас нет прав доступа к операции ' . (!isset($actionParams['name']) ? $actionParams['className']::$name : $actionParams['name']);
-                    } else {
-                        try {
-                            $result->successMsg = $actionParams['className']::groupAction($dataManager, $ids, $actionParams, !empty($_GET['adInfo']) ? $_GET['adInfo'] : []);
-                            $result->success = true;
-                        } catch (\Exception $e) {
-                            $result->content = $e->getMessage();
-                        }
-                    }
-                }
+    $result->content['rows'] = ob_get_contents();
+    ob_clean();
+
+    $result->content['pages'] = '';
+
+    if (isset($pages) && $pages) {
+      if ($pages) {
+        $pages->draw();
+        echo '<div style="background:#fff;">записей: <b>' . $pages->options['count'] . '</b>. страница <b>' . $pages->params['page'] . '</b> из <b>' . $pages->params['pages'] . '</b></div>';
+      }
+      $result->content['pages'] = ob_get_contents();
+      ob_end_clean();
+    }
+    $result->send();
+  }
+
+  public function loadCategorysAction() {
+    $result = new Server\Result();
+
+    ob_start();
+
+    $request = $this->parseRequest();
+
+    $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
+    $dataManager->drawCategorys();
+
+    $result->content = ob_get_contents();
+    ob_end_clean();
+
+    $result->send();
+  }
+
+  public function delRowAction() {
+
+    $request = $this->parseRequest();
+
+    $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
+
+    if ($dataManager->checkAccess()) {
+      $model = $request['modelName']::get($request['key'], $request['modelName']::index(), $request['params']);
+      if ($model) {
+        $model->delete($request['params']);
+      }
+    }
+    $result = new Server\Result();
+    $result->successMsg = empty($request['silence']) ? 'Запись удалена' : '';
+    $result->send();
+  }
+
+  public function updateRowAction() {
+
+    $request = $this->parseRequest();
+
+    $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
+
+    if ($dataManager->checkAccess()) {
+      $model = $request['modelName']::get($request['key'], $request['modelName']::index(), $request['params']);
+      if ($model) {
+        $model->{$request['col']} = $request['col_value'];
+        $model->save($request['params']);
+      }
+    }
+    $result = new Server\Result();
+    $result->successMsg = empty($request['silence']) ? 'Запись Обновлена' : '';
+    $result->send();
+  }
+
+  public function groupActionAction() {
+    $request = $this->parseRequest();
+    $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
+    $result = new Server\Result();
+    $result->success = false;
+    $result->content = 'Не удалось выполнить операцию';
+    if ($dataManager->checkAccess()) {
+      $ids = trim($request['ids'], ' ,');
+      if ($request['action'] && $ids) {
+        $actions = $dataManager->getActions();
+        if (!empty($actions[$request['action']])) {
+          $actionParams = $actions[$request['action']];
+          if (!empty($actionParams['access']['groups']) && !in_array(\Users\User::$cur->group_id, $actionParams['access']['groups'])) {
+            $result->content = 'У вас нет прав доступа к операции ' . (!isset($actionParams['name']) ? $actionParams['className']::$name : $actionParams['name']);
+          } else {
+            try {
+              $result->successMsg = $actionParams['className']::groupAction($dataManager, $ids, $actionParams, !empty($_GET['adInfo']) ? $_GET['adInfo'] : []);
+              $result->success = true;
+            } catch (\Exception $e) {
+              $result->content = $e->getMessage();
             }
-        } else {
-            $result->content = 'У вас нет прав доступа к менеджеру ' . $request['managerName'] . ' модели ' . $request['modelName'];
+          }
         }
-        $result->send();
+      }
+    } else {
+      $result->content = 'У вас нет прав доступа к менеджеру ' . $request['managerName'] . ' модели ' . $request['modelName'];
     }
+    $result->send();
+  }
 
-    public function delCategoryAction()
-    {
+  public function delCategoryAction() {
 
-        $request = $this->parseRequest();
+    $request = $this->parseRequest();
 
-        $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
+    $dataManager = new Ui\DataManager($request['modelName'], $request['managerName']);
 
-        if ($dataManager->checkAccess() && !empty($dataManager->managerOptions['categorys'])) {
-            $categoryModel = $dataManager->managerOptions['categorys']['model'];
-            $model = $categoryModel::get($request['key'], $categoryModel::index(), $request['params']);
-            if ($model) {
-                $model->delete($request['params']);
-            }
-        }
-        $result = new Server\Result();
-        $result->send();
+    if ($dataManager->checkAccess() && !empty($dataManager->managerOptions['categorys'])) {
+      $categoryModel = $dataManager->managerOptions['categorys']['model'];
+      $model = $categoryModel::get($request['key'], $categoryModel::index(), $request['params']);
+      if ($model) {
+        $model->delete($request['params']);
+      }
     }
+    $result = new Server\Result();
+    $result->send();
+  }
 
 }
