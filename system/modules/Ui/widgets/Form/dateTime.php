0e57e6dc581d36f3d0c816c85be8fc98d97c79bf
Alexey Krupsky <admin@inji.ru>
2016-11-24 02:23:23 +0700

    improove dateTime input generator

--- system/modules/Ui/widgets/Form/dateTime.php
+++ system/modules/Ui/widgets/Form/dateTime.php
@@ -1,28 +1,46 @@
 <?php
 App::$cur->libs->loadLib('JqueryUi');
+$uid = Tools::randomString();
+
 echo empty($options['noContainer']) ? '<div class="form-group">' : '';
 echo $label !== false ? "<label>{$label}</label>" : '';
-$uid = Tools::randomString();
-?>
-<input data-dateui="<?= $uid; ?>" type ="text" placeholder="<?= !empty($options['placeholder']) ? $options['placeholder'] : ''; ?>" class=" <?= !empty($options['class']) ? $options['class'] : 'form-control'; ?>" name = '<?= $name; ?>' value = '<?= (!empty($form->userDataTree[$name]) ? addcslashes($form->userDataTree[$name], "'") : (!empty($options['value']) ? addcslashes($options['value'], "'") : '')); ?>' />
-<?php
+$attributes = [
+    'type' => 'text',
+    'name' => $name,
+    'data-dateui' => $uid,
+    'class' => !empty($options['class']) ? $options['class'] : 'form-control',
+    'value' => !empty($options['value']) ? addcslashes($options['value'], "'") : (!empty($form->userDataTree[$name]) ? addcslashes($form->userDataTree[$name], "'") : '')
+];
+if (!empty($options['disabled'])) {
+  $attributes['disabled'] = 'disabled';
+}
+if (!empty($options['placeholder'])) {
+  $attributes['placeholder'] = $options['placeholder'];
+}
+if (!empty($options['checked'])) {
+  $attributes['checked'] = 'checked';
+}
+if (!empty($options['attributes'])) {
+  $attributes = array_merge($attributes, $options['attributes']);
+}
+echo Html::el('input', $attributes, '', null);
 echo!empty($options['helpText']) ? "<div class='help-block'>{$options['helpText']}</div>" : '';
 echo empty($options['noContainer']) ? '</div>' : '';
 ?>
 <script>
-    inji.onLoad(function () {
-      $("[data-dateui='<?= $uid; ?>']").datetimepicker({
-        changeMonth: true,
-        changeYear: true,
-        numberOfMonths: 1,
-        dateFormat: 'yy-mm-dd',
-        yearRange: "c-70:c+10",
-        timeFormat: 'HH:mm:ss',
-        beforeShow: function () {
-          setTimeout(function () {
-            $('.ui-datepicker').css('z-index', 99999999999999);
-          }, 500);
-        }
-      });
-    })
+  inji.onLoad(function () {
+    $("[data-dateui='<?= $uid; ?>']").datetimepicker({
+      changeMonth: true,
+      changeYear: true,
+      numberOfMonths: 1,
+      dateFormat: 'yy-mm-dd',
+      yearRange: "c-70:c+10",
+      timeFormat: 'HH:mm:ss',
+      beforeShow: function () {
+        setTimeout(function () {
+          $('.ui-datepicker').css('z-index', 99999999999999);
+        }, 500);
+      }
+    });
+  })
 </script>
