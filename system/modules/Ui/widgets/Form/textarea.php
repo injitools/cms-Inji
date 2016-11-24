e9956445be5947f379063159fcbe1cb692ad58b4
Alexey Krupsky <admin@inji.ru>
2016-11-24 23:30:07 +0700

    add disabled for textarea

--- system/modules/Ui/widgets/Form/textarea.php
+++ system/modules/Ui/widgets/Form/textarea.php
@@ -1,17 +1,19 @@
-  <?php
+<?php
 
-  echo empty($options['noContainer']) ? '<div class="form-group">' : '';
-  echo $label !== false ? "<label>{$label}</label>" : '';
-  $attributes = [
-      'name' => $name,
-      'class' => "form-control " . (!empty($options['class']) ? $options['class'] : '')
-  ];
-  if (!empty($options['required'])) {
-    $attributes['required'] = 'required';
-  }
-  if (!empty($options['attributes'])) {
-    $attributes = array_merge($attributes, $options['attributes']);
-  }
-  echo Html::el('textarea', $attributes, !empty($options['value']) ? $options['value'] : (!empty($form->userDataTree[$name]) ? addcslashes($form->userDataTree[$name], "'") : ''));
-  echo empty($options['noContainer']) ? '</div>' : '';
-  
+echo empty($options['noContainer']) ? '<div class="form-group">' : '';
+echo $label !== false ? "<label>{$label}</label>" : '';
+$attributes = [
+    'name' => $name,
+    'class' => "form-control " . (!empty($options['class']) ? $options['class'] : '')
+];
+if (!empty($options['required'])) {
+  $attributes['required'] = 'required';
+}
+if (!empty($options['disabled'])) {
+  $attributes['disabled'] = 'disabled';
+}
+if (!empty($options['attributes'])) {
+  $attributes = array_merge($attributes, $options['attributes']);
+}
+echo Html::el('textarea', $attributes, !empty($options['value']) ? $options['value'] : (!empty($form->userDataTree[$name]) ? addcslashes($form->userDataTree[$name], "'") : ''));
+echo empty($options['noContainer']) ? '</div>' : '';
