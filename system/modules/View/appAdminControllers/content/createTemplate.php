ed263b27edc43e6b095899ce7de7a1360f7d9ad2
Alexey Krupsky <admin@inji.ru>
2016-12-10 16:05:58 +0700

    TODO: blockdrop in template edit

--- system/modules/View/appAdminControllers/content/createTemplate.php
+++ system/modules/View/appAdminControllers/content/createTemplate.php
@@ -4,5 +4,4 @@ $form = new Ui\Form();
 $form->begin('Создание новой темы оформления');
 $form->input('text', 'name', 'Название темы');
 $form->input('hidden', 'map');
-$this->widget('View\blockDrop');
 $form->end('Создать', ['onclick' => 'blockDrop.submitMap(this);return false;']);
