38b614bb0b7d58798f2f51cb1410551fa53ae973
Alexey Krupsky <admin@inji.ru>
2016-12-20 16:19:24 +0700

    actual carts link

--- system/modules/Ecommerce/extensions/Dashboard/snippets/adminDashboardWidget/ecommerceStatistic.php
+++ system/modules/Ecommerce/extensions/Dashboard/snippets/adminDashboardWidget/ecommerceStatistic.php
@@ -1,27 +1,33 @@
 <?php
 return [
-    'widget' => function() {
+    'widget' => function () {
         ?>
         <div class="panel panel-default">
             <div class="panel-heading">Онлайн-магазин</div>
             <div class="panel-body">
                 <div class="row">
                     <div class="col-lg-6">
-                        <p>Всего товаров/отображаемых: <?= Ecommerce\Item::getCount(); ?> / <?= \App::$cur->ecommerce->getItemsCount(); ?></p>
-                        <p>Новых товаров/отображаемых сегодня: <?= Ecommerce\Item::getCount(['where' => ['date_create', date('Y-m-d 00:00:00'), '>']]); ?> / <?= \App::$cur->ecommerce->getItemsCount(['where' => ['date_create', date('Y-m-d 00:00:00'), '>']]); ?></p>
+                        <p>Всего товаров/отображаемых: <?= Ecommerce\Item::getCount(); ?>
+                            / <?= \App::$cur->ecommerce->getItemsCount(); ?></p>
+                        <p>Новых товаров/отображаемых
+                            сегодня: <?= Ecommerce\Item::getCount(['where' => ['date_create', date('Y-m-d 00:00:00'), '>']]); ?>
+                            / <?= \App::$cur->ecommerce->getItemsCount(['where' => ['date_create', date('Y-m-d 00:00:00'), '>']]); ?></p>
                     </div>
                     <div class="col-lg-6">
-                        <p>Всего Корзин/Заказов: <?= Ecommerce\Cart::getCount(); ?> / <?= Ecommerce\Cart::getCount(['where' => ['cart_status_id', 1, '>']]); ?></p>
-                        <p>Новых Корзин/Заказов сегодня:  <?= Ecommerce\Cart::getCount(['where' => [['date_create', date('Y-m-d 00:00:00'), '>']]]); ?> / <?= Ecommerce\Cart::getCount(['where' => [['cart_status_id', 1, '>'], ['complete_data', date('Y-m-d 00:00:00'), '>']]]); ?></p>                  
+                        <p>Всего Корзин/Заказов: <?= Ecommerce\Cart::getCount(); ?>
+                            / <?= Ecommerce\Cart::getCount(['where' => ['cart_status_id', 1, '>']]); ?></p>
+                        <p>Новых Корзин/Заказов
+                            сегодня: <?= Ecommerce\Cart::getCount(['where' => [['date_create', date('Y-m-d 00:00:00'), '>']]]); ?>
+                            / <?= Ecommerce\Cart::getCount(['where' => [['cart_status_id', 1, '>'], ['complete_data', date('Y-m-d 00:00:00'), '>']]]); ?></p>
                     </div>
                 </div>
             </div>
             <div class="panel-footer">
-                <a href ="/admin/Ecommerce/Item">Товары</a> |
-                <a href ="/admin/Ecommerce/Cart">Заказы</a>
+                <a href="/admin/Ecommerce/Item">Товары</a> |
+                <a href="/admin/Ecommerce/Cart?datamanagerFilters[cart_status_id][value]=2">Заказы</a>
             </div>
         </div>
         <?php
     }
-        ];
+];
         
