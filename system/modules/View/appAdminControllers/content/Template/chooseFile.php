562b57a31e5d073f9d2a981aa8cfeedb0555f613
Alexey Krupsky <admin@inji.ru>
2016-12-22 16:27:18 +0700

    code style

--- system/modules/View/appAdminControllers/content/Template/chooseFile.php
+++ system/modules/View/appAdminControllers/content/Template/chooseFile.php
@@ -1,11 +1,10 @@
 <h1>Файлы темы <?= $template->config['template_name']; ?></h1>
-<a href ='/admin/view/template/editFile/<?= $template->name; ?>?path=<?= $template->config['file']; ?>'>Основной файл темы</a>
+<a href='/admin/view/template/editFile/<?= $template->name; ?>?path=<?= $template->config['file']; ?>'>Основной файл
+    темы</a>
 <hr/>
 <?php
 foreach ($template->config['css'] as $file) {
     if (file_exists($template->path . '/css/' . $file)) {
-        ?>
-        <a href ='/admin/view/template/editFile/<?= $template->name; ?>?path=<?= 'css/' . $file; ?>'><?= $file; ?></a>
-        <?php
+        echo "<a href='/admin/view/template/editFile/{$template->name}?path=css/{$file}'><?= $file; ?></a> ";
     }
 }
