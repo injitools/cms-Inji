f6023401b67577e97ebeed7944792b611fb8b899
Alexey Krupsky <admin@inji.ru>
2017-02-14 00:06:44 +0700

    add active for parrent

--- system/modules/Menu/widgets/menu.php
+++ system/modules/Menu/widgets/menu.php
@@ -6,33 +6,41 @@ if (!empty($params[0])) {
 if (empty($code)) {
     $code = 'main';
 }
-$childDraws = function($item, $childDraws) {
+$childDraws = function($item, $childDraws, $activeFind) {
     if ($item->childs(['order' => ['weight', 'asc']])) {
         echo "<ul class ='list-unstyled'>";
         foreach ($item->childs(['order' => ['weight', 'asc']]) as $item) {
-            if (urldecode($_SERVER['REQUEST_URI']) == $item->href) {
+            if (urldecode($_SERVER['REQUEST_URI']) == $item->href || $activeFind($item, $activeFind)) {
                 $active = ' class = "active" ';
             } else {
                 $active = '';
             }
             echo "<li {$active}><a href = '{$item->href}'>{$item->name}</a>";
-            $childDraws($item, $childDraws);
+            $childDraws($item, $childDraws, $activeFind);
         }
         echo "</ul>";
     }
 };
-
+$activeFind = function($item, $activeFind) {
+    foreach ($item->childs(['order' => ['weight', 'asc']]) as $item) {
+        if (urldecode($_SERVER['REQUEST_URI']) == $item->href) {
+            return true;
+        }
+        return $activeFind($item, $activeFind);
+    }
+    return false;
+};
 
 $menu = \Menu\Menu::get($code, 'code');
 if ($menu) {
     foreach ($menu->items(['where' => ['parent_id', 0], 'order' => ['weight', 'asc']]) as $item) {
-        if (urldecode($_SERVER['REQUEST_URI']) == $item->href) {
+        if (urldecode($_SERVER['REQUEST_URI']) == $item->href || $activeFind($item, $activeFind)) {
             $active = ' class = "active" ';
         } else {
             $active = '';
         }
         echo "<li {$active}><a href = '{$item->href}'>{$item->name}</a>";
-        $childDraws($item, $childDraws);
+        $childDraws($item, $childDraws, $activeFind);
         echo "</li>";
     }
 }
--- system/modules/Menu/widgets/menuCollapse.php
+++ system/modules/Menu/widgets/menuCollapse.php
@@ -6,36 +6,45 @@ if (empty($code)) {
     $code = 'main';
 }
 $uid = Tools::randomString();
-$childDraws = function($item, $childDraws, $uid) {
+$childDraws = function($item, $childDraws, $uid, $activeFind) {
     if ($item->childs(['order' => ['weight', 'asc']])) {
         echo "<ul class ='dropdown-menu'>";
 
         foreach ($item->childs(['order' => ['weight', 'asc']]) as $item) {
-            if (urldecode($_SERVER['REQUEST_URI']) == $item->href) {
+            if (urldecode($_SERVER['REQUEST_URI']) == $item->href || $activeFind($item, $activeFind)) {
                 $active = ' class = "active" ';
             } else {
                 $active = '';
             }
             $childs = $item->childs(['order' => ['weight', 'asc']]);
             echo "<li {$active} " . ($childs ? 'class="dropdown"' : '') . "><a href = '{$item->href}' " . ($childs ? 'data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" class="multitogle' . $uid . '"' : '') . ">{$item->name}</a>";
-            $childDraws($item, $childDraws, $uid);
+            $childDraws($item, $childDraws, $uid, $activeFind);
         }
         echo "</ul>";
     }
+};
+$activeFind = function($item, $activeFind) {
+    foreach ($item->childs(['order' => ['weight', 'asc']]) as $item) {
+        if (urldecode($_SERVER['REQUEST_URI']) == $item->href) {
+            return true;
+        }
+        return $activeFind($item, $activeFind);
+    }
+    return false;
 };
 
 
 $menu = \Menu\Menu::get($code, 'code');
 if ($menu) {
     foreach ($menu->items(['where' => ['parent_id', 0], 'order' => ['weight', 'asc']]) as $item) {
-        if (urldecode($_SERVER['REQUEST_URI']) == $item->href) {
+        if (urldecode($_SERVER['REQUEST_URI']) == $item->href || $activeFind($item, $activeFind)) {
             $active = ' class = "active" ';
         } else {
             $active = '';
         }
         $childs = $item->childs(['order' => ['weight', 'asc']]);
         echo "<li {$active} " . ($childs ? 'class="dropdown"' : '') . "><a href = '{$item->href}' " . ($childs ? 'data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"' : '') . ">{$item->name}</a>";
-        $childDraws($item, $childDraws, $uid);
+        $childDraws($item, $childDraws, $uid, $activeFind);
         echo "</li>";
     }
 }
