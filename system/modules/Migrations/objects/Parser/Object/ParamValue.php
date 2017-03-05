606ce7680ecfa2432b16db586145f08d6a7176b8
Alexey Krupsky <admin@inji.ru>
2017-03-05 14:29:40 +0700

    add paramValue type for migration col

---
+++ system/modules/Migrations/objects/Parser/Object/ParamValue.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Parser Object Value
+ *
+ * @author Alexey Krupskiy <admin@inji.ru>
+ * @link http://inji.ru/
+ * @copyright 2015 Alexey Krupskiy
+ * @license https://github.com/injitools/cms-Inji/blob/master/LICENSE
+ */
+
+namespace Migrations\Parser\Object;
+
+class ParamValue extends \Migrations\Parser {
+
+    public function parse() {
+        $options = $this->param->options ? json_decode($this->param->options, true) : [];
+        $modelName = get_class($this->model);
+        $cols = $modelName::$cols;
+        $value = $this->data;
+        if (is_array($value)) {
+            $value = '';
+        }
+        $paramValue = \Ecommerce\Item\Param::get([[['item_id', $this->model->id], ['item_option_id', $this->param->value]]]);
+        if (!$paramValue) {
+            $paramValue = new \Ecommerce\Item\Param(['item_id' => $this->model->id, 'item_option_id' => $this->param->value]);
+        }
+        $paramValue->value = $value;
+        $paramValue->save();
+    }
+}
