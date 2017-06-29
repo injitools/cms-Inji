03fc86cc3f654eebb2c71021a9d2e6b03b8c062f
Alexey Krupsky <admin@inji.ru>
2017-06-29 15:01:17 +0700

    fix single warehouse

--- system/modules/Exchange1c/objects/Parser/Item/Offer/Warehouse.php
+++ system/modules/Exchange1c/objects/Parser/Item/Offer/Warehouse.php
@@ -16,27 +16,34 @@ class Warehouse extends \Migrations\Parser {
     public function parse() {
         if (is_array($this->data) && empty($this->data['@attributes'])) {
             foreach ($this->data as $warehouseCount) {
-                $count = $warehouseCount['@attributes']['КоличествоНаСкладе'];
-                $objectId = \App::$cur->migrations->findObject((string) $warehouseCount['@attributes']['ИдСклада'], 'Ecommerce\Warehouse');
-                if ($objectId) {
-                    $modelName = get_class($this->model);
-                    $warehouse = \Ecommerce\Item\Offer\Warehouse::get([[$modelName::index(), $this->model->pk()], [\Ecommerce\Warehouse::index(), $objectId->object_id]]);
-                    if (!$warehouse) {
-                        $warehouse = new \Ecommerce\Item\Offer\Warehouse([
-                            $modelName::index() => $this->model->pk(),
-                            \Ecommerce\Warehouse::index() => $objectId->object_id,
-                            'count' => $count
-                        ]);
-                        $warehouse->save();
-                    } else {
-                        if ($warehouse->count != $count) {
-                            $warehouse->count = $count;
-                            $warehouse->save();
-                        }
-                    }
+                $this->parseWarehouse($warehouseCount);
+            }
+        } elseif (is_array($this->data) && !empty($this->data['@attributes'])) {
+            $this->parseWarehouse($this->data);
+        }
+    }
+
+    function parseWarehouse($warehouseCount) {
+        $count = $warehouseCount['@attributes']['КоличествоНаСкладе'];
+        $objectId = \App::$cur->migrations->findObject((string)$warehouseCount['@attributes']['ИдСклада'], 'Ecommerce\Warehouse');
+        if ($objectId) {
+            $modelName = get_class($this->model);
+            $warehouse = \Ecommerce\Item\Offer\Warehouse::get([[$modelName::index(), $this->model->pk()], [\Ecommerce\Warehouse::index(), $objectId->object_id]]);
+            if (!$warehouse) {
+                $warehouse = new \Ecommerce\Item\Offer\Warehouse([
+                    $modelName::index() => $this->model->pk(),
+                    \Ecommerce\Warehouse::index() => $objectId->object_id,
+                    'count' => $count
+                ]);
+                $warehouse->save();
+            } else {
+                if ($warehouse->count != $count) {
+                    $warehouse->count = $count;
+                    $warehouse->save();
                 }
             }
         }
+
     }
 
 }
