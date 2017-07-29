e9a0099ae24b12704314a8645d45fbdb3484ae75
Alexey Krupsky <admin@inji.ru>
2017-07-29 19:47:32 +0700

    add params for widgets

--- system/modules/Sliders/info.php
+++ system/modules/Sliders/info.php
@@ -5,6 +5,14 @@ return [
     'requires' => [
         'Files'
     ],
+    'widgets' => [
+        'Sliders\slider' => [
+            'name' => 'Слайдер',
+            'params' => [
+                ['name'=>'Выберите', 'type' => 'select', 'source' => 'model', 'model' => 'Sliders\Slider']
+            ]
+        ]
+    ],
     'menu' => [
         'appAdmin' => [
             [
