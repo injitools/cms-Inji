57c32ac5b088d13a31055c912c35eec25749cd03
Alexey Krupsky <admin@inji.ru>
2017-08-25 11:38:16 +0700

    not auto redirecting, new systems view

--- system/modules/Money/appControllers/content/pay.php
+++ system/modules/Money/appControllers/content/pay.php
@@ -1,14 +1,9 @@
 <div class="money">
     <div class="content">
         <h2>Оплата счета №<?= $pay->id; ?></h2>
-        <h1 class="text-center">К оплате: <b><?= $pay->sum; ?> <?= $pay->currency->acronym(); ?></b></h1>
-        <h3>Выберите удобный способ оплаты и валюту</h3>
-        <table class="table table-striped table-bordered">
-            <tr>
-                <th>Способ оплаты</th>
-                <th>Валюта</th>
-                <th></th>
-            </tr>
+        <h1>К оплате: <b><?= $pay->sum; ?> <?= $pay->currency->acronym(); ?></b></h1>
+        <h3>Выберите удобный способ оплаты</h3>
+        <div class="row">
             <?php
             $linlcount = 0;
             $lastlink = '';
@@ -18,37 +13,29 @@
                     continue;
                 }
                 ?>
-                <tr>
-                    <td>
-                        <img src="<?= Statics::file($merchant->image ? $merchant->image->path : '/static/system/images/no-image.png', '150x150'); ?>"
+                <div class="col-md-4 col-sm-6 col-lg-3 text-center">
+                    <h4><?= $merchant->name(); ?></h4>
+                    <?php if ($merchant->image) { ?>
+                        <img src="<?= Statics::file($merchant->image->path, '150x150'); ?>"
                              class="img-responsive"/>
-                        <h4 class="text-center"><?= $merchant->name(); ?></h4>
-                    </td>
-                    <td>
-                        <?php
-                        foreach ($allowCurrencies as $allowCurrency) {
-                            $lastlink = "/money/merchants/go/{$pay->id}/{$merchant->id}/{$allowCurrency['currency']->id}";
-                            $linlcount++;
-                            $className = 'Money\MerchantHelper\\' . $merchant->object_name;
-                            $sum = $className::getFinalSum($pay, $allowCurrency);
-                            ?>
-                            <b><?= $allowCurrency['currency']->name(); ?></b>
-                            <a class="btn btn-primary"
-                               href="/money/merchants/go/<?= $pay->id; ?>/<?= $merchant->id; ?>/<?= $allowCurrency['currency']->id; ?>">Оплатить <?= $sum; ?> <?= $allowCurrency['currency']->acronym(); ?></a>
-                            <?php
-                        }
+                    <?php } ?>
+                    <?php
+                    foreach ($allowCurrencies as $allowCurrency) {
+                        $lastlink = "/money/merchants/go/{$pay->id}/{$merchant->id}/{$allowCurrency['currency']->id}";
+                        $linlcount++;
+                        $className = 'Money\MerchantHelper\\' . $merchant->object_name;
+                        $sum = $className::getFinalSum($pay, $allowCurrency);
                         ?>
-                    </td>
-                    <td width="100%">
-                        <?= $merchant->previewImage ? '<img src="' . $merchant->previewImage->path . '" class="img-responsive" />' : ''; ?>
-                    </td>
-                </tr>
+                        <a class="btn btn-primary btn-lg"
+                           href="/money/merchants/go/<?= $pay->id; ?>/<?= $merchant->id; ?>/<?= $allowCurrency['currency']->id; ?>">Оплатить <?= $sum; ?> <?= $allowCurrency['currency']->acronym(); ?></a>
+                        <?php
+                    }
+                    ?>
+                    <?= $merchant->previewImage ? '<img src="' . $merchant->previewImage->path . '" class="img-responsive" />' : ''; ?>
+                </div>
                 <?php
             }
-            if($linlcount===1){
-                Tools::redirect($lastlink);
-            }
             ?>
-        </table>
+        </div>
     </div>
 </div>
