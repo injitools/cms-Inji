87423acc0281b04dcbcf05192ccaa6db62acc182
Alexey Krupsky <admin@inji.ru>
2017-12-01 16:59:38 +0700

    tool for clean missing mediafiles

--- system/modules/Files/appAdminControllers/FilesController.php
+++ system/modules/Files/appAdminControllers/FilesController.php
@@ -14,4 +14,63 @@ class FilesController extends adminController {
         $this->view->page(['page' => 'blank']);
     }
 
+    public function clearMissingAction() {
+        if (!empty($_POST['files'])) {
+            var_dump($_POST['files']);
+            $files = \Files\File::getList(['where' => ['id', $_POST['files'], 'IN']]);
+            foreach ($files as $file) {
+                $file->delete();
+            }
+        }
+        $usedImages = [];
+        $installedModules = \Module::getInstalled(App::$primary);
+        foreach ($installedModules as $module) {
+            foreach (\Module::getModels($module) as $modelPath => $modelName) {
+                foreach ($modelName::$cols as $colName => $col) {
+                    if ($col['type'] == 'image') {
+                        $items = $modelName::getList(['where' => [$colName, 0, '!='], 'array' => true, 'key' => false, 'cols' => $modelName::colPrefix() . $colName]);
+                        if ($items) {
+                            foreach ($items as $item) {
+                                $usedImages[$item[$modelName::colPrefix() . $colName]] = true;
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+        echo '<form method="post"><table>';
+        $missingImages = \Files\File::getList(['where' => ['id', array_keys($usedImages), 'NOT IN'], 'key' => 'path', 'array' => true]);
+        $texts = '';
+        foreach (\Materials\Material::getList(['array' => true]) as $material) {
+            $texts .= $material['material_preview'];
+            $texts .= $material['material_text'];
+        }
+        if (!empty($installedModules['TextBlocks'])) {
+            foreach (\TextBlocks\Block::getList(['array' => true]) as $block) {
+                var_dump($block);
+                exit();
+                $texts .= $material['material_preview'];
+                $texts .= $material['material_text'];
+            }
+        }
+        $deleted = 0;
+        foreach ($missingImages as $path => $file) {
+            if (strpos($texts, $path)) {
+                unset($missingImages[$path]);
+                $deleted++;
+            }
+        }
+        foreach ($missingImages as $path => $file) {
+            echo '<tr>';
+            echo "<td><input type='checkbox' name='files[]' value='{$file['file_id']}' checked /></td>";
+            echo "<td>{$file['file_id']}</td>";
+            echo "<td>{$file['file_code']}</td>";
+            echo "<td>{$file['file_upload_code']}</td>";
+            echo "<td>{$file['file_name']}</td>";
+            echo '</tr>';
+        }
+        echo '</table><button>Удалить</button></form>';
+    }
+
 }
