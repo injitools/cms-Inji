502b5c70b0eaa4a8ffe922fd4d7c44a0dc8d38b0
Alexey Krupsky <admin@inji.ru>
2017-12-02 17:49:42 +0700

    show errors in manual restart

--- system/modules/Exchange1c/appAdminControllers/Exchange1cController.php
+++ system/modules/Exchange1c/appAdminControllers/Exchange1cController.php
@@ -11,6 +11,10 @@
 class Exchange1cController extends adminController {
 
     public function reExchangeAction() {
+        error_reporting(E_ALL);
+        ini_set('error_reporting', E_ALL);
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
         ini_set('memory_limit', '4000M');
         ignore_user_abort(true);
         set_time_limit(0);
