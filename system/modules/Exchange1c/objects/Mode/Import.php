e32788fd510a9a3348c92fcb92359cede2cc9370
Alexey Krupsky <admin@inji.ru>
2017-12-15 20:09:59 +0700

    fix

--- system/modules/Exchange1c/objects/Mode/Import.php
+++ system/modules/Exchange1c/objects/Mode/Import.php
@@ -10,13 +10,15 @@
  */
 
 namespace Exchange1c\Mode;
-
 class Import extends \Exchange1c\Mode {
 
     public function process() {
         $fileName = $_GET['filename'];
         $path = $this->exchange->path;
         \App::$cur->daemon->task(function () use ($fileName, $path) {
+            ini_set('memory_limit', '-1');
+            ignore_user_abort(true);
+            set_time_limit(0);
             \App::$cur->Migrations->startMigration(1, strpos($fileName, 'import') !== false ? 1 : 2, $path . '/' . $fileName);
         });
         echo 'success';
